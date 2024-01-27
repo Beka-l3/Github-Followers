ae3a078f8c5144458b56cfd442d8b8a8d94c4bc6
Bekzhan Talgat <bekzhantalgat01@gmail.com>
2024-01-27 20:39:23 +0300

    Project setup

---
+++ Github Followers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  Github Followers
+//
+//  Created by Bekzhan Talgat on 27.01.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPurple
+    }
+
+}
---
+++ Github Followers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  Github Followers
+//
+//  Created by Bekzhan Talgat on 27.01.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
