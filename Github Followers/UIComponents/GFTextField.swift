3ed957e492e0e9b375709e16c3827b518eb73afb
Beka-l3 <b.talgat@innopolis.university>
2024-01-27 22:45:24 +0300

    Create GFTextField

--- Github Followers/UIComponents/GFTextField.swift
+++ Github Followers/UIComponents/GFTextField.swift
@@ -7,3 +7,47 @@
 
 import UIKit
 
+
+final class GFTextField: UITextField {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+}
+
+
+extension GFTextField {
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        
+        textAlignment = .center
+        
+        font = .preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        
+        autocorrectionType = .no
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
