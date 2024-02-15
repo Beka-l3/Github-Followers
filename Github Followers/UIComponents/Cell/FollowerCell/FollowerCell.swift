e6c131367d3a6a1d1471fabc6a21423f5672532d
Beka-l3 <b.talgat@innopolis.university>
2024-02-15 18:04:20 +0300

    Refactor FollowerCell

--- Github Followers/UIComponents/Cell/FollowerCell/FollowerCell.swift
+++ Github Followers/UIComponents/Cell/FollowerCell/FollowerCell.swift
@@ -11,15 +11,11 @@ import SwiftUI
 final class FollowerCell: UICollectionViewCell {
     
     static let reuseID = "FollowerCellReuseID"
-    
-    lazy var avatarImageView = GFAvatarImageView(frame: .zero)
-    lazy var usernameLabel = GFTitleLabel(type: .first(alignment: .center), fontSize: Constants.usernameLabelFontSize)
-    
-    
+
+        
 //    MARK: lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
     }
     
     
@@ -31,46 +27,7 @@ final class FollowerCell: UICollectionViewCell {
 
 extension FollowerCell {
     
-    enum Constants {
-        static let padding:                 CGFloat     = 8
-        static let paddingM:                CGFloat     = 12
-        
-        static let usernameLabelFontSize:   CGFloat     = 16
-        static let usernameLabelHeight:     CGFloat     = 20
-    }
-}
-
-
-extension FollowerCell {
-    
-    private func configure() {
-        addSubviews(avatarImageView, usernameLabel)
-        
-        NSLayoutConstraint.activate([
-            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: Constants.padding),
-            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.padding),
-            avatarImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.padding),
-            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
-            
-            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: Constants.paddingM),
-            usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.paddingM),
-            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.paddingM),
-            usernameLabel.heightAnchor.constraint(equalToConstant: Constants.usernameLabelHeight)
-        ])
-    }
-}
-
-
-extension FollowerCell {
-    
     func set(follower: Follower) {
-        if #available(iOS 16, *) {
-            contentConfiguration = UIHostingConfiguration { FollowerView(follower: follower) }
-            
-        } else {
-            
-            usernameLabel.text = follower.login
-            avatarImageView.imageUrl = follower.avatarUrl
-        }
+        contentConfiguration = UIHostingConfiguration { FollowerView(follower: follower) }
     }
 }
